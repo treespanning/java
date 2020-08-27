2411dfc30d8a32a704669174578975436317cfd6
treespanning <2320591837@qq.com>
2020-08-27 23:48:05 +0800

    丑数：(2^x)*(3^y)*(5^m)

--- 丑数/src/Main.java
+++ 丑数/src/Main.java
@@ -27,6 +27,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(GetUglyNumber_Solution(78));
+        System.out.println(GetUglyNumber_Solution(36));
     }
 }
