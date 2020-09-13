023ebc6b9c478ee11a9745b4f35ad6ac91c86ea8
treespanning <2320591837@qq.com>
2020-09-13 17:23:05 +0800

    斐波那契数

--- Fibonacci/src/Main.java
+++ Fibonacci/src/Main.java
@@ -2,7 +2,7 @@
 import java.util.*;
 public class Main {
     private static Map<Integer,Integer> filter=new HashMap<>();
-    public static int Fibonacci(int n) {
+    public static int fibonacci(int n) {
         //f(n-1)+f(n-2)=f(n)
         if(0==n||1==n){
             return n;
@@ -11,14 +11,14 @@ public class Main {
         if(filter.containsKey(n-2)){
             ppre=filter.get(n-2);
         }else{
-            ppre=Fibonacci(n-2);
+            ppre=fibonacci(n-2);
             filter.put(n-2,ppre);
         }
         int pre=0;//第n-1斐波那契数
         if(filter.containsKey(n-1)){
             pre=filter.get(n-1);
         }else{
-            pre=Fibonacci(n-1);
+            pre=fibonacci(n-1);
             filter.put(n-1,pre);
         }
         return pre+ppre;
@@ -27,6 +27,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println(Fibonacci(n));
+        System.out.println(fibonacci(n));
     }
 }
